9521ebaca7ef4583b1cd7426ddd49425dfa7712c
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-02-25 11:39:03 -0400

    Added basic implementation of dolphin dive
    Still need to polish the mechanic, but it works on a functional level

--- Assets/DolphinDive.cs
+++ Assets/DolphinDive.cs
@@ -4,22 +4,57 @@ using UnityEngine.InputSystem;
 public class DolphinDive : MonoBehaviour
 {
     private AdvancedMoveController moveController;
+    private Rigidbody rb;
+
+    [SerializeField] private float forwardForce = 20f;
+    [SerializeField] private float upwardForce = 5f;
+    [SerializeField] private float cooldownTime = 0.2f;
+
+    private bool canDolphinDive = true;
+    private float lastDiveTime;
 
     private void Awake()
     {
         moveController = GetComponent<AdvancedMoveController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (moveController.isGrounded)
+        {
+            canDolphinDive = true;
+        }
     }
 
     public void OnDolphinDive()
     {
         if (moveController.isGrounded)
         {
-            Debug.Log("Jump pressed.");
+            return;
+        }
+        if (!canDolphinDive || Time.time < lastDiveTime + cooldownTime)
+        {
+            return;
         }
         else
         {
-            Debug.Log("Double jump would trigger");
+            ExecuteDive();
         }
 
+    }
+
+    private void ExecuteDive()
+    {
+        canDolphinDive = false;
+        lastDiveTime = Time.time;
+
+        Vector3 velocity = rb.linearVelocity;
+        velocity.y = 0f;
+        rb.linearVelocity = velocity;
+
+        Vector3 diveDirection = transform.forward * forwardForce + Vector3.up * upwardForce;
+
+        rb.AddForce(diveDirection, ForceMode.Impulse);
     }
 }
